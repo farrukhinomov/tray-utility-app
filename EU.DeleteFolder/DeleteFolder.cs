c3d4441444d7ded45a47d04eda94d5b44b53c8a3
Dilshod Niyozov <Dilshod@eshcom.com>
2020-03-13 15:57:28 +0500

    Created customizable delete output folder utility. Farrukh needs to fix the code and get rid of the goto.

---
+++ EU.DeleteFolder/DeleteFolder.cs
@@ -0,0 +1,71 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EU.DeleteFolder
+{
+    [Utility("Delete Output")]
+    public class DeleteFolder : UtilityBase
+    {
+        public static string dirName = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\DeleteOutput";
+
+        public override string Help()
+        {
+            return $"Deletes the folder that is specified in the config file. Path: {dirName}\\config.txt";
+        }
+
+        public override string Run()
+        {
+
+            var dir = new DirectoryInfo(dirName);
+            if (!dir.Exists)
+            {
+                dir.Create();
+            }
+            FileInfo file = new FileInfo(dir.FullName + "\\config.txt");
+            if (!file.Exists)
+            {
+                var sw = File.CreateText(file.FullName);
+                sw.Close();
+            }
+        comeHere: var possiblePath = File.ReadAllText(file.FullName).Trim();
+            if (string.IsNullOrEmpty(possiblePath))
+            {
+                var result = MessageBox.Show("Path to delete the folder is not specified. Would you like to set it?", "Path is not set", MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+                if (result == MessageBoxResult.Yes)
+                {
+                    var x = new Settings();
+                    x.ShowDialog();
+                    goto comeHere;
+                }
+                else
+                {
+                    return $"Path is not specified in the config file. Please go to {file.FullName} and specify the path to the folder you like to delete";
+                }
+            }
+            DirectoryInfo dirToDelete = new DirectoryInfo(possiblePath);
+            if (dirToDelete.Exists)
+            {
+                string status = "Folder \"" + possiblePath + "\" deleted successfully!";
+                try
+                {
+                    dirToDelete.Delete(true);
+                }
+                catch (Exception ex)
+                {
+                    status = $"There was a problem with deleting the folder:{possiblePath}. Exception: {ex.Message}";
+                }
+                return status;
+            }
+            else
+            {
+                return $"Folder {possiblePath} is either doesn't exist or already deleted.";
+            }
+        }
+    }
+}
---
+++ EU.DeleteFolder/Settings.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace EU.DeleteFolder
+{
+    public partial class Settings : Window
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var path = txtPath.Text;
+            var sw = File.CreateText(DeleteFolder.dirName + "\\config.txt");
+            sw.WriteLine(path);
+            sw.Flush();
+            sw.Close();
+            this.Close();
+        }
+    }
+}
