c7c37aa36c71eabe7403fef0a09d6f8817f22b25
Dilshod Komilov <komilovd@silkroadprofessionals.com>
2020-11-24 09:42:01 +0500

    add my changes

---
+++ UI.DeleteCache/DeleteCache.cs
@@ -0,0 +1,28 @@
+﻿using Common;
+using System.IO;
+
+namespace EU.DeleteCache
+{
+    [Utility("DeleteCache.dll")]
+    public class DeleteCache : UtilityBase
+    {
+        public override string Help()
+        {
+            return "Copies Microsoft.SqlServer.BatchParser.dll to Model Designer output";
+        }
+
+        public override string Run()
+        {
+            var path = @"C:\ProgramData\Eagle\Cache";
+            if (Directory.Exists(path))
+            {
+                Directory.Delete(path, true);
+                return "Cache has been deleted!";
+            }
+            else
+            {
+                return "Already deleted!";
+            }
+        }
+    }
+}
---
+++ UI.ModelDesignerActions/CopyBatchParser.cs
@@ -0,0 +1,27 @@
+﻿using Common;
+using System.IO;
+
+namespace EU.ModelDesignerActions
+{
+    [Utility("CopyBatchParser.dll")]
+    public class CopyBatchParser : UtilityBase
+    {
+        public override string Help()
+        {
+            return "Copies Microsoft.SqlServer.BatchParser.dll to Model Designer output";
+        }
+
+        public override string Run()
+        {
+            if (!File.Exists(@"C:\Repos\Eagle\Output\Debug\ModelDesigner\Demo\Microsoft.SqlServer.BatchParser.dll"))
+            {
+                File.Copy("Microsoft.SqlServer.BatchParser.dll", @"C:\Repos\Eagle\Output\Debug\ModelDesigner\Demo\Microsoft.SqlServer.BatchParser.dll");
+                return "Copied!";
+            }
+            else
+            {
+                return "File Already exists!";
+            }
+        }
+    }
+}
